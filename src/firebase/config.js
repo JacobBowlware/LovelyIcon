a5420343771520be95a047df66e2a674af7d6a00
Jacob Bowlware <jacobbowlware@gmail.com>
2023-05-05 10:28:46 -0500

    Setup  firebase.

---
+++ src/firebase/config.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBFHR_xBNW6kmO3EgUAL38niCS40w5oZfU",
+    authDomain: "lovelyicon-f3ad1.firebaseapp.com",
+    projectId: "lovelyicon-f3ad1",
+    storageBucket: "lovelyicon-f3ad1.appspot.com",
+    messagingSenderId: "437050328354",
+    appId: "1:437050328354:web:b184755ae5d055b4a7517b",
+    measurementId: "G-GG6HVE9N49"
+};
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
