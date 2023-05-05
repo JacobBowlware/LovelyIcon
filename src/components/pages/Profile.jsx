d7b3889b6de5e14464ee4ead39526dbce7de74f7
Jacob Bowlware <jacobbowlware@gmail.com>
2023-05-05 11:20:06 -0500

    Implement very basic profile page.

---
+++ src/components/pages/Profile.jsx
@@ -0,0 +1,41 @@
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+import { getAuth } from 'firebase/auth';
+import auth from '../../firebase/config';
+
+// Custom Components
+import TextHighlight from '../common/TextHighlight';
+
+const auth = getAuth(auth);
+
+const Profile = () => {
+    const navigate = useNavigate();
+
+
+    const handleLogout = (e) => {
+        e.preventDefault();
+
+        signOut(auth).then(() => {
+            navigate('/login');
+
+        }).catch((error) => {
+            // An error happened.
+            alert(error);
+        });
+    }
+
+    return (
+        <div className="container profile">
+            <h1 className="header-1 profile__header">Profile</h1>
+            <button
+                className="btn btn-primary logout__btn"
+                onClick={(e) => handleLogout(e)}
+            >
+                Logout
+            </button>
+        </div>
+    );
+}
+
+export default Profile;
